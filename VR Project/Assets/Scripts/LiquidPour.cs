ca7abed3dbb259d61bd11b9c8e496d27390ce475
Jae Han Song <rfv1479@gmail.com>
2022-05-19 17:27:21 +0900

    Update #9 :: Solve the Issue (Collider Glass)
    - 간혹 Cork가 있는데도 Pour Effect 발생
    - 원인은 Collider
    #3, #9

--- VR Project/Assets/Scripts/LiquidPour.cs
+++ VR Project/Assets/Scripts/LiquidPour.cs
@@ -13,12 +13,19 @@ public class LiquidPour : MonoBehaviour
         ACTIVE, NONACTIVE
     };
 
+    public enum AMOUNT
+    {
+        FULL, MIDDLE, EMPTY
+    }
+
     [SerializeField]
     private STATE bottleState = STATE.NONACTIVE;
     [SerializeField]
+    private AMOUNT bottleAmount = AMOUNT.FULL;
+    [SerializeField]
     private float boundaryHeight;
     [SerializeField]
-    private float speed = 0.1f;
+    private float speed = 0.2f;
     private LiquidVolume liquidVolume;
 
     void Awake()
@@ -29,23 +36,25 @@ public class LiquidPour : MonoBehaviour
     void Update()
     {
         statusCheck();
+        amountCheck();
     }
 
     private void statusCheck()
     {
         // check the activate status
         RaycastHit hit;
-        // Debug.DrawRay(transform.position, transform.up * 0.5f, Color.red);
+        Debug.DrawRay(transform.position, transform.up * 0.5f, Color.red);
 
         if (Physics.Raycast(transform.position, transform.up, out hit, 0.5f))
         {
-            // Debug.Log(hit.collider.gameObject.name);
+            Debug.Log(hit.collider.gameObject.name);
             bottleState =
                 hit.collider.gameObject.name == "Glass"
                 ? STATE.ACTIVE
                 : STATE.NONACTIVE;
         }
 
+        // You can pour if it's active
         if (bottleState == STATE.ACTIVE)
         {
             // condition check
@@ -59,11 +68,31 @@ public class LiquidPour : MonoBehaviour
         }
     }
 
+    private void amountCheck()
+    {
+        if (liquidVolume.level > 0.5f) bottleAmount = AMOUNT.FULL;
+        else if (liquidVolume.level > 0f) bottleAmount = AMOUNT.MIDDLE;
+        else bottleAmount = AMOUNT.EMPTY;
+    }
+
     private void bottlePourOn()
     {
-        // when we pour the liquid
-        if (liquidVolume.level >= 0) liquidVolume.level -= Time.deltaTime * speed;
-        else liquidVolume.level = 0;
+        if (bottleAmount == AMOUNT.FULL)
+        {
+            if (liquidVolume.level >= 0.5f)
+            {
+                liquidVolume.level -= Time.deltaTime * speed;
+            }
+            else liquidVolume.level = 0.5f;
+        }
+        else if (bottleAmount == AMOUNT.MIDDLE)
+        {
+            if (liquidVolume.level >= 0f)
+            {
+                liquidVolume.level -= Time.deltaTime * speed;
+            }
+            else liquidVolume.level = 0f;
+        }
 
         // Debug.Log(liquidVolume.level);
         liquidVolume.UpdateMaterialProperties();

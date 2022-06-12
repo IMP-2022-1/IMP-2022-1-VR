a811ec32fb4fdf047186a161f7b69eec7ffa7d77
Jae Han Song <rfv1479@gmail.com>
2022-06-12 09:12:17 +0900

    Feat/#9 : Test for UI

--- VR Project/Assets/Scripts/Lab (Liquid)/PotionController.cs
+++ VR Project/Assets/Scripts/Lab (Liquid)/PotionController.cs
@@ -10,17 +10,48 @@ public class PotionController : MonoBehaviour
     [SerializeField]
     private Transform BluePos;
 
+    [SerializeField]
+    private Transform PotionArea;
+
+    [SerializeField]
+    private Transform PotionCeiling;
+
+    public bool ready = false;
+    public bool ready2 = false;
+    public bool start = false;
 
     void OnCollisionEnter (Collision collision)
-    {
-        if (collision.gameObject.CompareTag("RedPotion"))
+    {   
+        if (collision.gameObject.CompareTag("RedPotion")) {
             PotionMove(RedPos, collision.gameObject);
-        if (collision.gameObject.CompareTag("BluePotion"))
+            ready = true;
+        }
+        if (collision.gameObject.CompareTag("BluePotion")) {
             PotionMove(BluePos, collision.gameObject);
+            ready2 = true;
+        }
     }
 
     void PotionMove(Transform pos, GameObject target) {
         target.transform.position = pos.position;
         target.transform.rotation = Quaternion.Euler(0,0,0);
+    }
+
+    void Update() {
+        if (ready && ready2 && !start) {
+            Debug.Log("Wat");
+            StartCoroutine("PotionEnded");
+            start = true;
+        }
+    }
+
+    IEnumerator PotionEnded() {
+        Debug.Log("Wow");
+        for (int i = 0; i < 130 ; i++) {
+            PotionCeiling.position += new Vector3(0.01f, 0, 0);
+            PotionArea.position -= new Vector3(0, 0.006f, 0);
+            yield return new WaitForSeconds(0.005f);
+        }
+        yield return null;
     }
 }

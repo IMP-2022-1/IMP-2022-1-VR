2a32a4a4cdfc09dda4f581dad25f12c109143b22
Jae Han Song <rfv1479@gmail.com>
2022-06-13 03:26:43 +0900

    Feat/#9 : resize the scene

---
+++ VR Project/Assets/Scripts/Lab (Liquid)/PuzzleLab.cs
@@ -0,0 +1,166 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PuzzleLab : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI RedArea;
+
+    [SerializeField]
+    private TextMeshProUGUI BlueArea;
+
+    [SerializeField]
+    private GameObject Red;
+
+    [SerializeField]
+    private GameObject Blue;
+
+    private int blue = 3; // blue is 3 (maximum)
+    private int red = 5; // red is 5 (maximum)
+
+    IEnumerator BlueHandle() {
+        for(int i = 0 ; i < 80 ; i++) {
+            Blue.transform.position += new Vector3(0, 0.005f, 0);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        for(int i = 0 ; i < 80 ; i++) {
+            Blue.transform.position -= new Vector3(0, 0.005f, 0);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        yield return null;
+    }
+
+    IEnumerator RedHandle() {
+        for(int i = 0 ; i < 80 ; i++) {
+            Red.transform.position += new Vector3(0, 0.005f, 0);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        for(int i = 0 ; i < 80 ; i++) {
+            Red.transform.position -= new Vector3(0, 0.005f, 0);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        yield return null;
+    }
+
+    IEnumerator BlueMove() {
+        for(int i = 0 ; i < 80 ; i++) {
+            Blue.transform.position += new Vector3(0, 0.005f, 0);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        for(int i = 0 ; i < 90 ; i++) {
+            Blue.transform.Rotate(1.0f, 0.0f, 0f, Space.World);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        yield return new WaitForSeconds(0.025f);
+
+        for(int i = 0 ; i < 90 ; i++) {
+            Blue.transform.Rotate(-1.0f, 0.0f, 0f, Space.World);
+            yield return new WaitForSeconds(0.005f);
+        }
+        
+        for(int i = 0 ; i < 80 ; i++) {
+            Blue.transform.position -= new Vector3(0, 0.005f, 0);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        yield return null;
+    }
+
+    IEnumerator RedMove() {
+        for(int i = 0 ; i < 80 ; i++) {
+            Red.transform.position += new Vector3(0, 0.005f, 0);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        for(int i = 0 ; i < 90 ; i++) {
+            Red.transform.Rotate(-1.0f, 0.0f, 0f, Space.World);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        yield return new WaitForSeconds(0.025f);
+
+        for(int i = 0 ; i < 90 ; i++) {
+            Red.transform.Rotate(1.0f, 0.0f, 0f, Space.World);
+            yield return new WaitForSeconds(0.005f);
+        }
+        
+        for(int i = 0 ; i < 80 ; i++) {
+            Red.transform.position -= new Vector3(0, 0.005f, 0);
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        yield return null;
+    }
+
+    void Start() {
+        ScoreCheck();
+    }
+
+    public void bluePour() {
+        StartCoroutine(BlueHandle());
+        ScoreCheck();
+        blue = 3;
+    }
+
+    public void redPour() {
+        StartCoroutine(RedHandle());
+        ScoreCheck();
+        red = 5;
+    }
+
+    public void blueThrow() {
+        StartCoroutine(BlueHandle());
+        ScoreCheck();
+        blue = 0;
+    }
+
+    public void redThrow() {
+        StartCoroutine(RedHandle());
+        ScoreCheck();
+        red = 0;
+    }
+
+    public void toRed() {
+        StartCoroutine(BlueMove());
+        if (blue + red < 5) {
+            red = blue + red;
+            blue = 0;
+        } else {
+            blue = (blue + red) - 5;
+            red = 5;
+        }
+
+        ScoreCheck();
+    }
+
+    public void toBlue() {
+        StartCoroutine(RedMove());
+        if (blue + red < 3) {
+            blue = blue + red;
+            red = 0;
+        } else {
+            red = (blue + red) - 3;
+            blue = 3;
+        }
+
+        ScoreCheck();
+    }
+
+    void ScoreCheck() {
+        RedArea.GetComponent<TextMeshProUGUI>().text = red.ToString();
+        BlueArea.GetComponent<TextMeshProUGUI>().text = blue.ToString();
+
+        if (red == 4 || blue == 4) {
+            // 문제 해결시 보상 코드 작성
+        }
+    }
+    
+}
---
+++ VR Project/Assets/Scripts/Lab (Liquid)/VideoLab.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoLab : MonoBehaviour
+{
+  [SerializeField]
+  private VideoPlayer video;
+  [SerializeField]
+  private VideoPlayer video2;
+
+  private GameObject Player;
+
+  void Start() {
+      Player = GameObject.Find("XR Origin");
+  }
+  
+  IEnumerator One()
+  {
+      video.Pause();
+      yield return new WaitForSeconds(10f);
+      video.Play();
+      yield return null;
+  }
+
+  IEnumerator Two()
+  {
+      yield return new WaitForSeconds(2f);
+      video2.Play();
+      yield return null;
+  }
+
+  public void QuizTwo()
+  {
+      StartCoroutine(Two());
+  }
+
+  void Update() {
+      Collider[] colls = Physics.OverlapSphere(transform.position, .25f);
+      Debug.Log(colls.Length);
+      if (colls.Length > 0)
+        StartCoroutine(One());
+  }
+}

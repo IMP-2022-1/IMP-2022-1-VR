2c616ec82b91f7413a8fd30ad91383007d19d70e
Jae Han Song <rfv1479@gmail.com>
2022-06-12 14:58:24 +0900

    Feat/#9 : Potion Status

--- VR Project/Assets/Scripts/Lab (Liquid)/PotionController.cs
+++ VR Project/Assets/Scripts/Lab (Liquid)/PotionController.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class PotionController : MonoBehaviour
 {
     [SerializeField]
-    private Transform RedPos;
-
-    [SerializeField]
-    private Transform BluePos;
-
-    [SerializeField]
     private Transform PotionArea;
 
     [SerializeField]
@@ -20,38 +14,20 @@ public class PotionController : MonoBehaviour
     public bool ready2 = false;
     public bool start = false;
 
-    void OnCollisionEnter (Collision collision)
-    {   
-        if (collision.gameObject.CompareTag("RedPotion")) {
-            PotionMove(RedPos, collision.gameObject);
-            ready = true;
-        }
-        if (collision.gameObject.CompareTag("BluePotion")) {
-            PotionMove(BluePos, collision.gameObject);
-            ready2 = true;
-        }
-    }
-
-    void PotionMove(Transform pos, GameObject target) {
-        target.transform.position = pos.position;
-        target.transform.rotation = Quaternion.Euler(0,0,0);
-    }
-
     void Update() {
         if (ready && ready2 && !start) {
-            Debug.Log("Wat");
             StartCoroutine("PotionEnded");
             start = true;
         }
     }
 
     IEnumerator PotionEnded() {
-        Debug.Log("Wow");
         for (int i = 0; i < 130 ; i++) {
             PotionCeiling.position += new Vector3(0.01f, 0, 0);
             PotionArea.position -= new Vector3(0, 0.006f, 0);
             yield return new WaitForSeconds(0.005f);
         }
+        
         yield return null;
     }
 }
